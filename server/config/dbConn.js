792c56083fba2bf6afafd2fd6ba9c5a6921f95b6
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2023-08-17 12:53:59 +0700

    Update dbConn.js

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -5,7 +5,7 @@ exports.connectDB = () => {
     mongoose
       // .connect(process.env.MONGO_DATABASE_URI_UBUNTU, {
       .connect(
-        "mongodb://kreangsak:572110@51.79.240.206:27017/registerDB?authSource=admin",
+        "mongodb://username:password@xx.xx.xx.xx:27017/registerDB?authSource=admin",
         {
           useNewUrlParser: true,
           useUnifiedTopology: true,

e925cea6b6b278f5840af076c456d6fc05080010
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2023-08-17 18:53:16 +0700

    Update dbConn.js

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
+const UserCounter = require("../models/UserCounter");
+const UserAdmin = require("../models/UserAdmin");
+
+const initializeCounters = async () => {
+  const projectIds = [
+    "establishment01_id",
+    "establishment02_id",
+    "establishment03_id",
+  ];
+
+  for (let _id of projectIds) {
+    await UserCounter.insertInitialData(_id);
+  }
+};
 
 exports.connectDB = () => {
   try {
@@ -15,7 +29,10 @@ exports.connectDB = () => {
         console.log("Successfully connected to database");
 
         // Create Username Admin
-        // UserAdmin.insertInitialData();
+        UserAdmin.insertInitialData();
+        
+        // Create Counter Defalute 0
+        initializeCounters();
       });
   } catch (err) {
     console.log(err);

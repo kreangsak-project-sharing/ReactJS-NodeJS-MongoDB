cc65e47107a8828bb44c80d1c40d05e3e037b16c
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2023-08-17 19:14:24 +0700

    Update userController.js

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,22 @@
+const userEstablishment01 = require("../models/Establishment01");
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = async (req, res) => {
+  // Get all users from MongoDB
+  const users = await userEstablishment01.find().lean();
+
+  // If no users
+  if (!users?.length) {
+    return res.status(400).json({ message: "No users found" });
+  }
+
+  res.json(users);
+};
+
 const getData = (req, res) => {
   const { id } = req.params;
 };
 
-module.exports = { getData };
+module.exports = { getData, getAllUsers };
